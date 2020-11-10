7f1b3d8c6f9ff246d8caf5a616b7cb292dce45f7
EC2 Default User <ec2-user@ip-172-31-47-22.ec2.internal>
2020-11-10 13:17:12 +0000

    [Add] aboutページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  scope module: :public do
+  root 'homes#top'
+  get 'about' => "homes#about"
+
+  devise_for :customers
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
